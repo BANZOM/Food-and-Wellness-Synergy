f88eda6c2b1bcc2b8d083202f60d783ddb3137b6
Aditya Panwar <adityap@cabin4j.com>
2024-03-02 16:06:48 +0530

    stage changes

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import model
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'dump/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/process', methods=['POST', 'GET'])
+@app.route('/process_text', methods=['POST', 'GET'])
 def process():
     if request.method == 'POST':
         user_input = request.form['user_input']
@@ -15,6 +17,18 @@ def process():
         return render_template('result.html', user_input=user_input, result=result)
     else:
         return render_template('process.html')
+
+@app.route('/process_image', methods=['POST', 'GET'])
+def process_image():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and model.allowed_file(file.filename):
+            filename = file.filename
+            file.save(UPLOAD_FOLDER + '/' + filename)
+            result = model.dummy_process(filename)
+            return render_template('result.html', user_input=filename, result=result)
+    else:
+        return render_template('image_upload.html')
 
 if __name__ == '__main__':
     app.run("0.0.0.0",5000,debug=True)
--- model.py
+++ model.py
@@ -1,2 +1,8 @@
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
+
 def dummy_process(input):
     return input[::-1]
+
+def dummy_process_image(filename):
+    return f"Image {filename} processed"
