036a3c4470b8b43a6b06711e92d551dcf7ad89b7
Abhishek Verma <vermaabhimanyu593@gmail.com>
2024-03-02 14:36:08 +0530

    saved

--- AddItem/app.py
+++ AddItem/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
 
-
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route("/", methods=['GET', 'POST'])
 def addItems():
