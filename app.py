e02afb522c76c937687f9139012ac0a416dd5c91
Aditya Panwar <adityap@cabin4j.com>
2024-03-03 08:37:48 +0530

    Add index route and handle invalid requests in get_recipe

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/get_recipe_from_text', methods=['POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_recipe_from_text', methods=['POST', 'GET'])
 def get_recipe():
     if request.method == 'POST':
-        text = request.form['input_text']
+        text = request.json.get('input_text','')
         processed_text = model.get_recipe(text)
         return jsonify({'processed_text': processed_text})
-
+    else:
+        return jsonify({'error': 'Invalid request'})
+    
 if __name__ == '__main__':
     app.run("0.0.0.0",5002,debug=True)
